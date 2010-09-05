ecb5a4c6ef59dfa702e3f87486f844ae9223a933
Conor Hunt <conor.hunt+git@gmail.com>
2010-09-05 17:32:20 -0400

    cleaned up code

--- recipes/cap_server_configs.rb
+++ recipes/cap_server_configs.rb
@@ -1,34 +1,35 @@
-require 'helpers'
+require File.dirname(__FILE__) + '/helpers'
 
 namespace :server_configs do
   desc "Check for modified configuration files, replace them with the local version and restart services"
   task :replace, :only => { :manage_configs => true } do
-    helper = CapServerConfig::Helper.new
-    cap_hosts = helper.get_all_cap_hosts
-    cap_hosts.each do |host|
+    helper = CapServerConfigs::Helper.new(self)
+    helper.get_cap_hosts.each do |host|
       helper.host = host
-      files = helper.get_modified_config_files
-      files.each do |file_info|
-        helper.create_remote_backup(file_info)
-        helper.replace_remote_file(file_info)
-        helper.restart_service(file_info)
+      helper.get_modified_config_files.each do |file_info|
+        puts "#{file_info[:local_path]} differs from #{file_info[:remote_path]}\n"
+        helper.create_remote_backup(file_info[:remote_path])
+        helper.replace_remote_file(file_info[:local_file], file_info[:remote_path])
+        helper.restart_service(file_info[:local_file])
       end
     end
+    puts output
   end
 
   desc "Check for modified configuration files"
   task :check, :only => { :manage_configs => true } do
-    helper = CapServerConfig::Helper.new
-    cap_hosts = helper.get_all_cap_hosts
-    cap_hosts.each do |host|
+    helper = CapServerConfigs::Helper.new(self)
+    output = ""
+    helper.get_cap_hosts.each do |host|
       helper.host = host
-      files = helper.get_modified_config_files
-      files.each do |file_info|
-         puts "#{file_info[:local_path]} differs from #{file[:remote_path]}"
-         File.open("/tmp/local_config","w") {|f| f << file_info[:local] }
+      helper.get_modified_config_files.each do |file_info|
+         output += "\n"
+         output += "#{file_info[:local_path]} differs from #{file_info[:remote_path]}\n"
+         File.open("/tmp/local_config","w") {|f| f << helper.strip_cap_server_config_commands(file_info[:local]) }
          File.open("/tmp/remote_config","w") {|f| f << file_info[:remote] }
-         puts `git diff -w /tmp/local_config /tmp/remote_config`
+         output += `diff -w -B -u /tmp/local_config /tmp/remote_config`
       end
     end
+    puts output
   end
 end
--- recipes/helpers.rb
+++ recipes/helpers.rb
@@ -1,16 +1,23 @@
 module CapServerConfigs
   class Helper
     attr_accessor :host
+    attr_accessor :cap
 
-    def get_all_cap_hosts
-      run('hostname') do |channel, stream, data|
+    def initialize(cap)
+      self.cap = cap
+    end
+
+    def get_cap_hosts
+      cap_hosts = []
+      cap.run('hostname') do |channel, stream, data|
         cap_hosts << channel[:server].instance_variable_get(:@host)
       end
+      cap_hosts
     end
 
     def strip_cap_server_config_commands(file)
-      file = file.gsub(/#\s*location:\s*.+\n|^\s*\n/, '')
-      file.gsub(/#\s*restart:\s*.+\n|^\s*\n/, '')
+      file = file.gsub(/#\s*location:\s*.+\n/, '')
+      file.gsub(/#\s*restart:\s*.+\n/, '')
     end
 
     def compare_files(local_file, remote_file)
@@ -21,42 +28,44 @@ module CapServerConfigs
 
     def get_modified_config_files
       modified_files = []
-      @hostname = capture('hostname', :hosts => @host).strip
+      hostname = cap.capture('hostname', :hosts => @host).strip
       dir = "config/servers/#{hostname}"
-      Dir["#{dir}/**/*"].each do |file|
-        next if File.directory?(file)
+      Dir["#{dir}/**/*"].each do |local_path|
+        next if File.directory?(local_path)
 
-        local_file = File.read(file)
-        remote_location = (local_file.match(/#\s*location:\s*(.+)$/)[1] rescue nil)
-        if remote_location.nil? or remote_location.length == 0
-          puts "Could not find location: setting in configuration file #{file}"
+        local_file = File.read(local_path)
+        remote_path = (local_file.match(/#\s*location:\s*(.+)$/)[1] rescue nil)
+        if remote_path.nil? or remote_path.length == 0
+          puts "Could not find location: setting in configuration file #{local_path}"
           next
         end
 
+        remote_file = cap.capture("cat #{remote_path}", :hosts => @host)
+
         if !compare_files(local_file, remote_file)
-          modified_files << local_file
+          modified_files << {:local => local_file, :remote => remote_file, :local_path => local_path, :remote_path => remote_path}
         end
       end
 
       modified_files
     end
 
-    def replace_remote_file
-      puts "replacing remote file #{remote_location}"
-      put(local_file, remote_location, :host => @host)
+    def replace_remote_file(local_file, remote_path)
+      puts "replacing remote file #{remote_path}"
+      cap.put(local_file, remote_path, :host => @host)
     end
 
     def create_remote_backup(remote_file_path)
-      backup_file = remote_file_path + ".cap_bak"
-      puts "saving backup of #{remote_file_path} to #{backup_file}"
-      run('cp #{remote_file_path} #{backup_file}', :hosts => @host)
+      backup_file_path = remote_file_path + ".cap_bak"
+      puts "saving backup of #{remote_file_path} to #{backup_file_path}"
+      cap.run('cp #{remote_file_path} #{backup_file_path}', :hosts => @host)
     end
 
     def restart_service(local_file)
       restart_command = (local_file.match(/#\s*restart:\s*(.+)$/)[1] rescue nil)
       if restart_command
         puts "restarting remote service"
-        run(remote_command, :hosts => @host)
+        cap.run(remote_command, :hosts => @host)
       end
     end
   end
